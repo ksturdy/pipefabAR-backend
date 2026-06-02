b387e9387397709ab402d2c3b0b1340047719079
Kipp Sturdivant <sturdy4@gmail.com>
2026-06-01 20:00:11 -0500

    Remove database init from startup - let migrate endpoint handle it

--- server.js
+++ server.js
@@ -68,15 +68,9 @@ app.post('/migrate', async (req, res) => {
 });
 
 const startServer = async () => {
-  try {
-    await initializeDatabase();
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (err) {
-    console.error('Failed to start server:', err);
-    process.exit(1);
-  }
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 };
 
 startServer();
